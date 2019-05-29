0e0faa68d2c2ca8ed39122e9f2d32f7b5d111c14
George Suveti <george.suveti@irian.ro>
2019-05-29 20:57:37 +0300

    authorization server: remove http security config

--- spring-authorization/src/main/java/com/example/springauthorization/SecurityConfig.java
+++ spring-authorization/src/main/java/com/example/springauthorization/SecurityConfig.java
@@ -31,16 +31,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.authorizeRequests().antMatchers("/login").permitAll()
-                .antMatchers("/oauth/token/revokeById/**").permitAll()
-                .antMatchers("/tokens/**").permitAll()
-                .anyRequest().authenticated()
-                .and().formLogin().permitAll()
-                .and().csrf().disable();
-        // @formatter:on
-    }
 
 }
